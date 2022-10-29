1653dd2f554c095fb647fd7bb6c959ff2a89c4f1
teo <phanthanhhieuu@gmail.com>
2022-10-30 01:42:00 +0700

    update project

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,77 +1,69 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import './Header.css'
-import { Link } from "react-router-dom";
-
+import { Link } from 'react-router-dom'
 
 function Header() {
-  const toggleMenu = () => {
-    const navContainer = document.getElementById('navbarSupportedContent');
-    navContainer.classList.toggle('show-nav');
-    return navContainer;
-  }
+
   return (
-  //   <header className="header_section">
-  //   <div className="container-fluid">
-  //     <nav className="navbar navbar-expand-lg custom_nav-container ">
-  //       <Link className="navbar-brand" to='/'>
-  //         <span>
-  //           BOOKDISCOVERY
-  //         </span>
-  //       </Link>
-  //       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-  //       <div className='nav-items'>
-  //         <div className="nav-close">
-  //         <h3 onClick={toggleMenu}>&#10539;</h3>
-  //         </div>
-  //         <ul className="navbar-nav">
-  //           <li className="nav-item">
-  //           <Link className="nav-link" to='/'> Trang chủ </Link>
-  //           </li>
-  //           <li className="nav-item">
-  //             <Link className="nav-link" to='/sanpham'> Sản phẩm </Link>
-  //           </li>
-  //           <li className="nav-item active">
-  //             <Link className="nav-link" to='/thongtin'> Thông tin</Link>
-  //           </li>
-  //           <li className="nav-item">
-  //             <Link className="nav-link" to='/lienhe'>Liên hệ</Link>
-  //           </li>
-  //         </ul>
-  //         <div className="user_option-box">
-  //           <Link to='/user'>
-  //             <i className="fa fa-user" aria-hidden="true" />
-  //           </Link>
-  //           <Link to='/cart'>
-  //             <i className="fa fa-cart-plus" aria-hidden="true" />
-  //           </Link>
-  //         </div>
-  //       </div>
-  //       </div>
-  //       <div className="menu-icon">
-  //       <h3 onClick={toggleMenu}>&#9776;</h3>
-  //     </div>
-  //     </nav>
-  //   </div>
-  // </header>
-  <div className='header_container'>
-      <div className='header_logo'>
-        <span>BOOKDISCOVERY</span>
+    <>
+      {/* // Header top */}
+      <div className="header-middle">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4">
+              <div className="logo pull-left">
+                <a href="/">
+                  <img src="/assets/images/home/logo1.gif" alt="" />
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-8">
+              <div className="shop-menu pull-right">
+                <ul className="nav navbar-nav collapse navbar-collapse">
+                  <li className='dropdown'>
+                    <a className='Setting-item'>
+                      <i className="fa fa-user dropbtn"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <Link to={"/cart"}>
+                      <i className="fa fa-shopping-cart" />
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='header_content'>
-      <ul className='header_list'>
-        <li className='header-item'>Trang chủ</li>
-        <li className='header-item'>Sản phẩm</li>
-        <li className='header-item'>Thông tin</li>
-        <li className='header-item'>Liên hệ</li>
-      </ul>
-      </div>
-      <div className='header_icon-user'>
-        <i className="fa fa-user" aria-hidden="true" />
-      </div>
-      <div className='header_icon-cart'>
-        <i className='fa fa-cart-plus' aria-hidden="true" />
-      </div>
-  </div>
+      {/* // Header bottom */}
+      <div className="header-bottom">
+        <div className="container">
+          <div className="row header-bot">
+            <div className="col-sm-8">
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+              </div>
+
+            </div>
+            <div className="col-sm-4">
+              <div className="search_box pull-right">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  onChange={(e) => this.props.setSearchText(e.target.value)}
+                  onKeyUp={(e) => this.handeSearch(e.keyCode)} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div></>
   )
 }
 
